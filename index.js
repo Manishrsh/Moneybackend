c43e18dc038e1d49f97d4e2f25ffd8852805f433
Manishrsh <manishrs5235@gmail.com>
2024-08-10 19:04:05 +0530

    ci create 12 harikirat 1

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ mongoose.connect(dbURI, {
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send("hello Money World cicd nginx");
+  res.send("hello Money World cicd nginx not");
 });
 
 // Date formatting function
