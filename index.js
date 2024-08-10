bf6f1973beaa8ac6419b2c0097a6167e7d0e0ae1
Manishrsh <manishrs5235@gmail.com>
2024-08-10 07:51:14 +0530

    ci create 8

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ mongoose.connect(dbURI, {
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send("hello Money World cicd");
+  res.send("hello Money World cicd ok");
 });
 
 // Date formatting function
