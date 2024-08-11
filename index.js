b054d6a254c599215a42d60de3d0e51f25ba68f1
Manishrsh <manishrs5235@gmail.com>
2024-08-11 12:47:35 +0530

    ci create 12 harikirat ok

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const Expence = require('./model/expencemodel');
 const AddMoney = require('./model/addmoney');
 
 app.use(cors({
-  origin: 'https://money-front-one.vercel.app' // Correct origin
+  origin: 'https://money-front-manishrs5235-gmailcoms-projects.vercel.app' // Correct origin
 }));
 
 const dbURI = 'mongodb+srv://manishrs5235:z1vJjUxF4pOVBcIa@cluster0.mjowunr.mongodb.net/mydatabase?retryWrites=true&w=majority&appName=Cluster0';
